0a00bb0a40262cb0d706f57963192bea803f1998
demetrio <demetrio@g4flex.com.br>
2012-05-24 13:11:45 -0300

    Initial commit

---
+++ src/main/java/org/openexchange/oer_java/Currencies.java
@@ -0,0 +1,49 @@
+package org.openexchange.oer_java;
+
+/**
+ * All available currencies on {@link http://openexchangerates.org/currencies.json}
+ * 
+ * @author Demétrio C. Menezes Neto
+ * 
+ */
+public enum Currencies {
+	/**
+	 * United Arab Emirates Dirham
+	 */
+	AED,
+	/**
+	 * Afghan Afghani
+	 */
+	AFN,
+	/**
+	 * Albanian Lek
+	 */
+	ALL,
+	/**
+	 * Armenian Dram
+	 */
+	AMD,
+	/**
+	 * Netherlands Antillean Guilder
+	 */
+	ANG,
+	/**
+	 * Angolan Kwanza
+	 */
+	AOA,
+	/**
+	 * Argentine Peso
+	 */
+	ARS,
+	/**
+	 * Australian Dollar
+	 */
+	AUD, 
+	/**
+	 * Aruban Florin
+	 */
+	AWG, 
+	//TODO Documentate all currencies
+	AZN, BAM, BBD, BDT, BGN, BHD, BIF, BMD, BND, BOB, BRL, BSD, BTN, BWP, BYR, BZD, CAD, CDF, CHF, CLF, CLP, CNY, COP, CRC, CUP, CVE, CZK, DJF, DKK, DOP, DZD, EGP, ETB, EUR, FJD, FKP, GBP, GEL, GHS, GIP, GMD, GNF, GTQ, GYD, HKD, HNL, HTK, HTG, HUF, IDR, IEP, ILS, INR, IQD, IRR, ISK, JMD, JOD, JPY, KES, KGS, KHR, KMF, KPW, KRW, KWD, KZT, LAK, LBP, LKR, LRD, LSL, LTL, LVL, LYD, MAD, MDL, MGA, MDK, MMK, MNT, MOP, MRO, MUR, MVR, MWK, MXN, MZN, NAD, NGN, NIO, NOK, NPR, NZD, OMR, PAB, PEN, PGK, PHP, PKR, PLN, PYG, QAR, RON, RSD, RUB, RWF, SAR, SBD, SCR, SDG, SEK, SGD, SHP, SLL, SOS, SRD, STD, SVC, SYP, SZL, THB, TJS, TMT, TND, TOP, TRY, TTD, TWD, TZS, UAH, UGX, USD, UYU, UZS, VEF, VND, VUV, WST, XAF, XCD, XDR, XOF, XPF, YER, ZAR, ZMK, ZWD, ZWL
+
+}
